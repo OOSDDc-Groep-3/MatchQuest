5f73e9df3c4a52f931cdb552af783e2aa9370757
Carlos <79849894+6Waifu9@users.noreply.github.com>
2025-12-04 15:58:34 +0100

    user gegevens opgehaald uit database en op de userprofile pagina weergegeven

--- src/MatchQuest.App/ViewModels/UserProfileVIewModel.cs
+++ src/MatchQuest.App/ViewModels/UserProfileVIewModel.cs
@@ -1,6 +1,66 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using MatchQuest.Core.Models;
+
 namespace MatchQuest.App.ViewModels;
 
-public class UserProfileViewModel
+public partial class UserProfileViewModel : ObservableObject
 {
-    
+    private readonly GlobalViewModel _global;
+
+    public UserProfileViewModel(GlobalViewModel global)
+    {
+        _global = global;
+    }
+
+    public string Name
+    {
+        get => _global.Client?.Name ?? string.Empty;
+        set
+        {
+            if (_global.Client != null)
+            {
+                _global.Client.Name = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public DateTime BirthDate
+    {
+        get => _global.Client?.BirthDate ?? DateTime.Now;
+        set
+        {
+            if (_global.Client != null)
+            {
+                _global.Client.BirthDate = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public string Region
+    {
+        get => _global.Client?.Region ?? string.Empty;
+        set
+        {
+            if (_global.Client != null)
+            {
+                _global.Client.Region = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public string Biography
+    {
+        get => _global.Client?.Bio ?? string.Empty;
+        set
+        {
+            if (_global.Client != null)
+            {
+                _global.Client.Bio = value;
+                OnPropertyChanged();
+            }
+        }
+    }
 }
